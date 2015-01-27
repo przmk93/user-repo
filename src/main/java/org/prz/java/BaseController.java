b7facdb210b26af80600a2aa99b8f4ad2a8250e4
Andrzej Bolanowski <andrzejbolanowski@gmail.com>
2015-01-27 19:37:40 +0100

    Initial commit

---
+++ src/main/java/org/prz/java/BaseController.java
@@ -0,0 +1,33 @@
+package main.java.org.prz.java;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BaseController {
+	Integer counter = null;
+
+	private Integer userCounter(){
+		if (counter != null)
+			return ++counter;
+		else
+			return counter = 1;
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+		model.addAttribute("userCounter", userCounter());
+		model.addAttribute("message", "Welcome");
+		return "index";
+	}
+	
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	public String welcomeName(ModelMap model, @PathVariable String name) {
+		model.addAttribute("message", name);
+		model.addAttribute("userCounter", userCounter());
+		return "index";
+	}
+}
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+     <h1>Prz project - User controller</h1>
+     <h3>Message : ${message}</h3>
+     <h3>Licznik odwiedzin : ${userCounter}</h3>
+</body>
+</html>
